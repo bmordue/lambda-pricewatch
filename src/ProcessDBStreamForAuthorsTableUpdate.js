48e95a003c06147d29018ea107e81ba635ad2f38
Ben Mordue <benmordue@gmail.com>
2017-03-21 21:44:48 +0000

    Publish individual SNS messages for each newly-added author

--- src/ProcessDBStreamForAuthorsTableUpdate.js
+++ src/ProcessDBStreamForAuthorsTableUpdate.js
@@ -1,4 +1,25 @@
+var SNS = require('aws-sdk/clients/sns');
+
 exports.lambda_handler = function(event, context, callback) {
-    console.log(JSON.stringify(event, null, 2));
-    callback(null, "success");
+  var sns = new SNS();
+
+  event.Records.forEach(function(record) {
+    if (record.eventName == "INSERT") {
+      try {
+        var author = record.dynamodb.NewImage.name.S;
+      } catch (e) {
+        return callback(e, "Error extracting author name from update stream");
+      }
+      sns.publish({
+          TopicArn: "arn:aws:sns:eu-west-1:285774518219:author_added",
+          Message: author
+      }, function(err, data) {
+        if (err) {
+          console.log("Error publishing to topic: " + err);
+        } else {
+          console.log("Sent message for author " + author);
+        }
+      });
+    }
+  });
 };
