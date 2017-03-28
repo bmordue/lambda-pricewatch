0a0ce1b72ca82f16051d2f69e10d20458adbd6b5
Ben Mordue <benmordue@gmail.com>
2017-03-28 21:59:29 +0100

    Move inline policy doc to file

--- terraform/iam_roles.tf
+++ terraform/iam_roles.tf
@@ -1,39 +1,11 @@
 resource "aws_iam_role" "lambda-basic-execution" {
   name = "lambda-basic-execution-role"
-
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "lambda.amazonaws.com"
-            }
-        }
-    ]
-}
-EOF
+  assume_role_policy = file("lambda_execution_policy.json")
 }
 
 resource "aws_iam_role" "lambda-with-full-sns" {
   name = "lambda-with-full-sns-role"
-
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "lambda.amazonaws.com"
-            }
-        }
-    ]
-}
-EOF
+  assume_role_policy = file("lambda_execution_policy.json")
 }
 
 resource "aws_iam_policy_attachment" "AWSLambdaDynamoDBExecutionRole-attach" {
