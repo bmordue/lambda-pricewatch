ec7454fb4e65522b3d19809659f0dd3634632342
Ben Mordue <benmordue@gmail.com>
2017-03-28 21:38:32 +0100

    Boilerplate lamdba functions in TF

---
+++ terraform/RefreshPriceForAllTitles.tf
@@ -0,0 +1,17 @@
+resource "aws_lambda_function" "RefreshPriceForAllTitles" {
+  filename         = "../target/RefreshPriceForAllTitles.zip"
+  function_name    = "RefreshPriceForAllTitles"
+  role             = "${aws_iam_role.lambda-basic-execution-role.arn}"
+  handler          = "exports.lambda_handler"
+  runtime          = "${var.node_runtime}"
+  source_code_hash = "${base64sha256(file("../target/RefreshPriceForAllTitles.zip"))}"
+
+  environment {
+    variables = {
+      AMZN_SERVICE_HOST = "${var.amzn_service_host}"
+      AMZN_ACCESS_KEY_ID = "${var.amzn_access_key_id}"
+      AMZN_ACCESS_KEY_SECRET = "${var.amzn_access_key_secret}"
+      AMZN_ASSOCIATE_TAG = "${var.amzn_associate_tag}"
+    }
+  }
+}
---
+++ terraform/RefreshPriceForTitle.tf
@@ -0,0 +1,17 @@
+resource "aws_lambda_function" "RefreshPriceForTitle" {
+  filename         = "../target/RefreshPriceForTitle.zip"
+  function_name    = "RefreshPriceForTitle"
+  role             = "${aws_iam_role.lambda-basic-execution-role.arn}"
+  handler          = "exports.lambda_handler"
+  runtime          = "${var.node_runtime}"
+  source_code_hash = "${base64sha256(file("../target/RefreshPriceForTitle.zip"))}"
+
+  environment {
+    variables = {
+      AMZN_SERVICE_HOST = "${var.amzn_service_host}"
+      AMZN_ACCESS_KEY_ID = "${var.amzn_access_key_id}"
+      AMZN_ACCESS_KEY_SECRET = "${var.amzn_access_key_secret}"
+      AMZN_ASSOCIATE_TAG = "${var.amzn_associate_tag}"
+    }
+  }
+}
