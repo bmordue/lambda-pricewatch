a59032060f9c0d42b18522f982d980b2aa2ff583
Ben Mordue <benmordue@gmail.com>
2017-03-31 12:59:55 +0100

    Roles for Dynamodb and SNS access

--- terraform/iam_roles.tf
+++ terraform/iam_roles.tf
@@ -8,6 +8,17 @@ resource "aws_iam_role" "lambda-with-full-sns" {
   assume_role_policy = "${file("lambda_execution_policy.json")}"
 }
 
+resource "aws_iam_role" "lambda-with-full-sns-and-dynamodb" {
+  name = "lambda-with-full-sns-role"
+  assume_role_policy = "${file("lambda_execution_policy.json")}"
+}
+
+resource "aws_iam_role" "lambda-with-full-dynamodb" {
+  name = "lambda-with-full-sns-role"
+  assume_role_policy = "${file("lambda_execution_policy.json")}"
+}
+
+
 resource "aws_iam_policy_attachment" "AWSLambdaDynamoDBExecutionRole-attach" {
   name       = "AWSLambdaDynamoDBExecutionRole-attachment"
   roles      = ["{aws_iam_role.lambda-basic-execution.name}"]
@@ -17,6 +28,16 @@ resource "aws_iam_policy_attachment" "AWSLambdaDynamoDBExecutionRole-attach" {
 resource "aws_iam_policy_attachment" "AmazonSNSFullAccess-attach" {
   name       = "AmazonSNSFullAccess-attachment"
   roles      = ["{aws_iam_role.lambda-basic-execution.name}",
-                "{aws_iam_role.lambda-with-full-sns.name}"]
+                "{aws_iam_role.lambda-with-full-sns.name}",
+                "{aws_iam_role.lambda-with-full-sns-and-dynamodb.name}"]
   policy_arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
 }
+
+resource "aws_iam_policy_attachment" "AmazonDynamoDBFullAccess-attach" {
+  name       = "AmazonDynamoDBFullAccess-attachment"
+  roles      = ["{aws_iam_role.lambda-with-full-dynamodb.name}",
+                "{aws_iam_role.lambda-with-full-sns-and-dynamodb.name}"]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+# TODO: more granular roles -- nothing should have full access
