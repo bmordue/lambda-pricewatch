1f2f47a0eca9202cc23d1cab7f7d4724f4bfab5c
Ben Mordue <benmordue@gmail.com>
2017-03-31 12:59:20 +0100

    Provision Dynamodb tables

---
+++ terraform/tables.tf
@@ -0,0 +1,36 @@
+resource "aws_dynamodb_table" "pricewatch_titles" {
+  name           = "pricewatch_titles"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "Title"
+  range_key      = "Price"
+
+  stream_enabled = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  attribute {
+    name = "Title"
+    type = "S"
+  }
+  
+  attribute {
+    name = "Price"
+    type = "N"
+  }
+}
+
+resource "aws_dynamodb_table" "pricewatch_authors" {
+  name           = "pricewatch_authors"
+  read_capacity  = 10
+  write_capacity = 10
+  hash_key       = "Name"
+
+  stream_enabled = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  attribute {
+    name = "Name"
+    type = "S"
+  }
+}
+
