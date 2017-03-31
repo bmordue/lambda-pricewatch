d67d4fdc787b2d9d86cd975d5bec366a8d4afb21
Ben Mordue <benmordue@gmail.com>
2017-03-31 13:58:08 +0100

    Fix var substitution

--- terraform/iam_roles.tf
+++ terraform/iam_roles.tf
@@ -21,22 +21,22 @@ resource "aws_iam_role" "lambda-with-full-dynamodb" {
 
 resource "aws_iam_policy_attachment" "AWSLambdaDynamoDBExecutionRole-attach" {
   name       = "AWSLambdaDynamoDBExecutionRole-attachment"
-  roles      = ["{aws_iam_role.lambda-basic-execution.name}"]
+  roles      = ["${aws_iam_role.lambda-basic-execution.name}"]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaDynamoDBExecutionRole"
 }
 
 resource "aws_iam_policy_attachment" "AmazonSNSFullAccess-attach" {
   name       = "AmazonSNSFullAccess-attachment"
-  roles      = ["{aws_iam_role.lambda-basic-execution.name}",
-                "{aws_iam_role.lambda-with-full-sns.name}",
-                "{aws_iam_role.lambda-with-full-sns-and-dynamodb.name}"]
+  roles      = ["${aws_iam_role.lambda-basic-execution.name}",
+                "${aws_iam_role.lambda-with-full-sns.name}",
+                "${aws_iam_role.lambda-with-full-sns-and-dynamodb.name}"]
   policy_arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
 }
 
 resource "aws_iam_policy_attachment" "AmazonDynamoDBFullAccess-attach" {
   name       = "AmazonDynamoDBFullAccess-attachment"
-  roles      = ["{aws_iam_role.lambda-with-full-dynamodb.name}",
-                "{aws_iam_role.lambda-with-full-sns-and-dynamodb.name}"]
+  roles      = ["${aws_iam_role.lambda-with-full-dynamodb.name}",
+                "${aws_iam_role.lambda-with-full-sns-and-dynamodb.name}"]
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
 
--- terraform/tables.tf
+++ terraform/tables.tf
@@ -24,6 +24,7 @@ resource "aws_dynamodb_table" "pricewatch_authors" {
   read_capacity  = 10
   write_capacity = 10
   hash_key       = "Name"
+  range_key      = "Name"
 
   stream_enabled = true
   stream_view_type = "NEW_AND_OLD_IMAGES"
