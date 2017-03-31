f529417d73640e28ad0fa518a102faee782d8e86
Ben Mordue <benmordue@gmail.com>
2017-03-31 12:58:57 +0100

    Tidy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,6 @@
-
-#dynamodb tables
-
-#sns topics
-
-#sns subscriptions
-
-#iam roles
-
-#lambda functions
-
-##eventstream mappings
-##env vars
-
-#policies
-
-#kms key 
-
 provider "aws" {
   profile    = "${var.cli_profile}"
   region     = "${var.region}"
 }
+
+#TODO: KMS key
