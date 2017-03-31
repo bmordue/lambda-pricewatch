2b70ee3024f60de27aa25a58a69fe6ea6ac0f018
Ben Mordue <benmordue@gmail.com>
2017-03-31 12:04:19 +0100

    Rename functions to make intent clear

---
+++ terraform/RequestPriceRefreshForAllTitles.tf
@@ -0,0 +1,15 @@
+resource "aws_lambda_function" "RequestPriceRefreshForAllTitles" {
+  filename         = "../target/RequestPriceRefreshForAllTitles.zip"
+  function_name    = "RequestPriceRefreshForAllTitles"
+  role             = "${aws_iam_role.lambda-with-full-sns-and-dynamodb.arn}"
+  handler          = "exports.lambda_handler"
+  runtime          = "${var.node_runtime}"
+  source_code_hash = "${base64sha256(file("../target/RequestPriceRefreshForAllTitles.zip"))}"
+
+  environment {
+    variables = {
+      TITLES_TABLE_NAME = "${aws_dynamodb_table.pricewatch_titles.id}"
+      TITLE_REFRESH_TOPIC_ARN = "${aws_sns_topic.title_refresh.arn}"
+    }
+  }
+}
---
+++ terraform/RequestTitlesRefreshForAllAuthors.tf
@@ -0,0 +1,15 @@
+resource "aws_lambda_function" "RequestTitlesRefreshForAllAuthors" {
+  filename         = "../target/RequestTitlesRefreshForAllAuthors.zip"
+  function_name    = "RequestTitlesRefreshForAllAuthors"
+  role             = "${aws_iam_role.lambda-with-full-sns-and-dynamodb.arn}"
+  handler          = "exports.lambda_handler"
+  runtime          = "${var.node_runtime}"
+  source_code_hash = "${base64sha256(file("../target/RequestTitlesRefreshForAllAuthors.zip"))}"
+
+  environment {
+    variables = {
+      AUTHORS_TABLE_NAME = "${aws_dynamodb_table.pricewatch_authors.id}"
+      AUTHOR_REFRESH_TOPIC_ARN = "${aws_sns_topic.author_refresh.arn}"
+    }
+  }
+}
