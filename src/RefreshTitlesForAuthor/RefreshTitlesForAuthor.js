535aac091f5163ac29359cedca5be164d937ce87
Ben Mordue <benmordue@gmail.com>
2017-04-14 14:14:58 +0100

    Refresh titles for author

--- src/RefreshTitlesForAuthor/RefreshTitlesForAuthor.js
+++ src/RefreshTitlesForAuthor/RefreshTitlesForAuthor.js
@@ -1,5 +1,9 @@
-
+var async = require("async");
 var aws = require("aws-lib");
+var aws_sdk = require("aws-sdk");
+var db = new aws_sdk.DynamoDB();
+var util = require("util");
+var prodAdvClient;
 
 exports.lambda_handler = function(event, context, callback) {
     var keyId = process.env.AMZN_ACCESS_KEY_ID;
@@ -7,28 +11,112 @@ exports.lambda_handler = function(event, context, callback) {
     var associateTag = process.env.AMZN_ASSOCIATE_TAG;
     var amazonServiceHost = process.env.AMZN_SERVICE_HOST;
 
-    var prodAdvClient = aws.createProdAdvClient(keyId, keySecret, associateTag, { host: amazonServiceHost});
+    prodAdvClient = aws.createProdAdvClient(keyId, keySecret, associateTag, { host: amazonServiceHost});
 
-    event.Records.forEach(function(record) {
-        var authorName;
-        try {
-            authorName = record.Sns.Message;
-        } catch (e) {
-            return callback(e, "Error extracting author name from SNS message");
+    async.each(event.Records, handleNotification, function(err) {
+        callback(err, "done");
+    });
+};
+
+function handleNotification(record, callback) {
+    var authorName;
+    try {
+        authorName = record.Sns.Message;
+    } catch (e) {
+        return callback(new Error("Failed to extract author name from SNS message"));
+    }
+    console.log("Refresh requested for author " + authorName);
+    var params = {
+        Author: authorName,
+        SearchIndex: "KindleStore",
+        ResponseGroup: "ItemIds"
+    };
+    getSearchResultsPage(params, 1, function(err, resultsPage) {
+        if (err) {
+            console.log("Error making request to ProdAdv API: " + err);
+            return callback(err);
         }
-        var params = {
-            Author: authorName,
-            ItemPage: 1,
-            SearchIndex: "KindleStore",
-            ResponseGroup: "Small"
-        };
-        prodAdvClient.call("ItemSearch", params, function(err, result) {
+        console.log(util.format("Got %s results for %s", resultsPage.Items.TotalResults, authorName));
+        
+        var pages = pageNumbersAsList(resultsPage.Items.TotalPages);
+
+        handleResultsPage(resultsPage, function(err) {
             if (err) {
-                return callback(err, "Error making request to ProdAdv API");
+                console.log(util.format("Error processing results for page 1: %j", err));
             }
-            console.log("Got result from ProdAdv for " + authorName);
-            console.log(JSON.stringify(result, null, 2));
+            async.each(pages, function(page, each_cb) {
+                getSearchResultsPage(params, page, function(err, result) {
+                    if (err) {
+                        console.log(util.format("Error making request to ProdAdv API: %j", err));
+                        return each_cb(); // do not propagate error, to allow other pages to be processed
+                    }
+                    handleResultsPage(result, function(err) {
+                        if (err) {
+                            console.log(util.format("Error processing results for page %s: %j", page, err));
+                        }
+                        each_cb(); // do not propagate error, to allow other pages to be processed
+                    });
+                });
+            }, callback);
         });
     });
-    callback(null, "success");
-};
+}
+
+// http://stackoverflow.com/a/10050831/567039
+// ProdAdv results page numbering starts at 1
+// page 1 (first) is dealt with separately, so we want [2..maxPage]
+function pageNumbersAsList(maxPage) {
+    return Array.apply(null, Array(maxPage - 1)).map(function (_, i) {return i + 2;});
+}
+
+function getSearchResultsPage(params, page, callback) {
+    params.ItemPage = page;
+    prodAdvClient.call("ItemSearch", params, callback);
+}
+
+function handleResultsPage(resultsPage, callback) {
+    async.each(resultsPage.Items.Item, addTitleToDbIfNotPresent, callback);
+}
+
+function prepareDynamoParams(item) {
+    var params = {
+        TableName: process.env.TITLES_TABLE_NAME,
+        ConditionExpression: "attribute_not_exists(ASIN)",
+        Item: {
+            "ASIN": {
+                S: item.ASIN
+            },
+            "DetailPageURL": {
+                S: item.DetailPageURL
+            },
+            "ItemAttributes": {
+                M: {
+                    "Author": {
+                        S: item.ItemAttributes.Author
+                    },
+                    "Manufacturer": {
+                        S: item.ItemAttributes.Manufacturer
+                    },
+                    "Title": {
+                        S: item.ItemAttributes.Title
+                    }
+                }
+            }
+        }
+    };
+    return params;
+}
+
+function addTitleToDbIfNotPresent(item, callback) {
+    var params = prepareDynamoParams(item);
+    db.putItem(params, function(err, data) {
+        if (err) {
+            console.log("Error putting item in DB");
+            console.log(err, err.data);
+        } else {
+            console.log("Successfully put item in DB");
+            console.log(data);
+        }
+        callback(); // do not propagate error
+    });
+}
