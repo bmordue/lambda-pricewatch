73b408a8c4ffb2b1d8ddd14df5e3d29147a2376e
Ben Mordue <benmordue@gmail.com>
2017-04-14 14:13:54 +0100

    Deploy script

---
+++ deploy/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set +x
+
+PROJECT_DIR="$(dirname "$(realpath $0)")"/..
+SRC_DIR=$PROJECT_DIR/src
+TARGET_DIR=$PROJECT_DIR/target
+DEPLOY_DIR=$PROJECT_DIR/deploy
+TF_DIR=$PROJECT_DIR/terraform
+CUR_DIR=$(pwd)
+
+echo "Remove built artifacts at $TARGET_DIR"
+rm -r $TARGET_DIR
+
+echo 'Install npm dependencies'
+cd $SRC_DIR
+for d in $(ls -1)
+do
+  cd $d
+  npm install
+  cd ..
+done
+    
+echo 'Zip sources'
+cd $DEPLOY_DIR
+sh ./zip_sources.sh
+
+echo 'Terraform apply'
+cd $TF_DIR
+terraform apply
+
+echo 'Done'
+cd $CUR_DIR
+
